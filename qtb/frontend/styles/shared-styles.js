367c093fa91d22f1d6052ec7dd6da8fcfbb8c7cf
Iskan <andrea.belkhir@yahoo.it>
2020-11-16 12:36:52 +0100

    primo commit

---
+++ qtb/frontend/styles/shared-styles.js
@@ -0,0 +1,36 @@
+// eagerly import theme styles so as we can override them
+import '@vaadin/vaadin-lumo-styles/all-imports';
+
+const $_documentContainer = document.createElement('template');
+
+$_documentContainer.innerHTML = `
+<custom-style>
+  <style>
+    html {
+    }
+  </style>
+</custom-style>
+
+<dom-module id="button-style" theme-for="vaadin-button">
+  <template>
+    <style>
+      :host(:not([theme~="tertiary"])) {
+        background-image: linear-gradient(var(--lumo-tint-5pct), var(--lumo-shade-5pct));
+        box-shadow: inset 0 0 0 1px var(--lumo-contrast-20pct);
+      }
+
+      :host(:not([theme~="tertiary"]):not([theme~="primary"]):not([theme~="error"]):not([theme~="success"])) {
+        color: var(--lumo-body-text-color);
+      }
+
+      :host([theme~="primary"]) {
+        text-shadow: 0 -1px 0 var(--lumo-shade-20pct);
+      }
+    </style>
+  </template>
+</dom-module>
+
+
+`;
+
+document.head.appendChild($_documentContainer.content);
